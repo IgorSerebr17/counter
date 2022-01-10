ccbaad197d431e999f9971a4b7a7885218824dc5
IgorSerebr17 <38539739+IgorSerebr17@users.noreply.github.com>
2022-01-11 00:05:16 +0300

    Add files via upload

---
+++ end-without-hw/src/components/countersList.jsx
@@ -0,0 +1,60 @@
+/** @format */
+
+import React, { useState } from "react";
+import Counter from "./counter";
+
+const CountersList = () => {
+  const initialState = [
+    { id: 0, value: 0, name: "Ненужная вещь" },
+    { id: 1, value: 4, name: "Ложка" },
+    { id: 2, value: 0, name: "Вилка" },
+    { id: 3, value: 0, name: "Тарелка" },
+    { id: 4, value: 0, name: "Набор минималиста" },
+  ];
+
+  const [counters, setCounters] = useState(initialState);
+  const handleDelete = (id) => {
+    const newCounters = counters.filter((c) => c.id !== id);
+    setCounters(newCounters);
+  };
+  const onIncrement = (id) => {
+    const b = counters.map((item) => {
+      if (item.id === id) {
+        item.value += 1;
+      }
+      return item;
+    });
+    setCounters(b);
+  };
+  const onDecrement = (id) => {
+    const b = counters.map((item) => {
+      if (item.id === id) {
+        item.value = item.value - 1;
+      }
+      return item;
+    });
+    setCounters(b);
+  };
+  const handleReset = () => {
+    setCounters(initialState);
+    console.log("handle reset");
+  };
+
+  return (
+    <>
+      {counters.map((count) => (
+        <Counter
+          key={count.id}
+          onDelete={handleDelete}
+          onIncrement={onIncrement}
+          onDecrement={onDecrement}
+          {...count}
+        />
+      ))}
+      <button className='btn btn-primary btn-sm m-2' onClick={handleReset}>
+        Сброс
+      </button>
+    </>
+  );
+};
+export default CountersList;
